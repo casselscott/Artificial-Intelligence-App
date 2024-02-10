eb2ac69c63338a9a767764d4a54fcefa352ab1e3
casselscott <casselscott.e@outlook.com>
2024-02-10 14:22:47 -0700

    setting changes

--- facerecognition/settings.py
+++ facerecognition/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-dcik%wlwkxkfuf@t0nr9*ep_%1inrnn*)uj8-i@1xm!^2@p-#6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '.vercel.app', 'face recognition', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = [ '.vercel.app',  '127.0.0.1', 'localhost']
 
 
 # Application definition
